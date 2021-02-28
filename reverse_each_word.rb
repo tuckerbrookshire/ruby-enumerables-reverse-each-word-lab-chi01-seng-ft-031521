2fed282a9c400c74c659626bd567249e0badac0e
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-28 16:35:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word
+def reverse_each_word(sentence1)
  string_array = ("Hello there, and how are you?").split
  new_string = string_array.collect { |i| i.reverse}
  new_string.join(" ")
